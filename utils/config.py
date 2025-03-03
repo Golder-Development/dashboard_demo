6ec5e240d7a2b2c010feae93ba3babfd47da4df1
PaulGolder-DBC <pgolder1972@gmail.com>
2025-03-03 14:48:29 +0000

    Pdpy correction

--- utils/config.py
+++ utils/config.py
@@ -17,6 +17,19 @@ LOG_LEVEL = "ERROR"
 
 DIRECTORIES = {  # "directory_name": "directory_path"
     "BASE_DIR": Path(os.getcwd()),
+    "data_dir": os.path.join("data"),
+    "output_dir": os.path.join("output"),
+    "logs_dir": os.path.join("logs"),
+    "reference_dir": os.path.join( "reference_files"),
+    "components_dir": os.path.join( "components"),
+    "app_pages_dir": os.path.join( "app_pages"),
+    "utils_dir": os.path.join( "utils"),
+    "source_dir": os.path.join( "source"),
+    "tests_dir": os.path.join( "tests"),
+}
+
+DIRECTORIES_original = {  # "directory_name": "directory_path"
+    "BASE_DIR": Path(os.getcwd()),
     "data_dir": os.path.join(BASE_DIR, "data"),
     "output_dir": os.path.join(BASE_DIR, "output"),
     "logs_dir": os.path.join(BASE_DIR, "logs"),
